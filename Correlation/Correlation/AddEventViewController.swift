a94b8dc4a5e93176337efa44932903f8c5f35152
Robert Fryman <robert.fryman@gmail.com>
2015-06-08 17:30:10 -0500

    Add two Controllers
    Add Event View Controller
    Add Question View Controller

---
+++ Correlation/Correlation/AddEventViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddEventController.swift
+//  Correlation
+//
+//  Created by Robert Fryman on 6/5/15.
+//  Copyright (c) 2015 Centresource. All rights reserved.
+//
+
+import Cocoa
+
+class AddEventController: UIViewController {
+
+}
